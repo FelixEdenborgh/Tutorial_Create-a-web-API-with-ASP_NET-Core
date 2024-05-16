d52c3a3495df4733ac87483d7147e156befc464c
FelixEdenborgh <Felix@edenborgh.se>
2024-05-16 15:35:51 +0200

    Add project files.

---
+++ Tutorial_Create a web API with ASP_NET Core/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Tutorial_Create_a_web_API_with_ASP_NET_Core
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
